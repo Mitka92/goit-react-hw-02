423dfc015091239b6c0d0b32f955dcb962a72203
Mitka92 <mashkovskiy1992@gmail.com>
2024-07-22 16:48:46 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Section from './components/Section/Section';
+
+function App() {
+  return (
+    <>
+      <Section>
+        <h1 className="title">goit-react-hw-02</h1>
+      </Section>
+    </>
+  );
+}
+
+export default App;
