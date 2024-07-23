7ae0315e198fa37de8fd26d7bee08d680a6aae66
Mitka92 <mashkovskiy1992@gmail.com>
2024-07-23 15:50:28 +0300

    upd

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,8 +3,13 @@ import css from './Feedback.module.css';
 
 function Feedback({ feedback, totalFeedback }) {
   const { good, neutral, bad } = feedback;
+  const positive = Math.round((good / totalFeedback) * 100);
   return (
-    <ul className={clsx(css['feedback-list'])}>
+    <ul
+      className={clsx(css['feedback-list'], {
+        [css['reset-active']]: totalFeedback > 0,
+      })}
+    >
       <li className={clsx(css['feedback-item'])}>
         <p className={clsx(css['feedback-text'])}>Good: {good}</p>
       </li>
@@ -17,10 +22,13 @@ function Feedback({ feedback, totalFeedback }) {
       <li className={clsx(css['feedback-item'])}>
         <p className={clsx(css['feedback-text'])}>Total: {totalFeedback}</p>
       </li>
-      <li className={clsx(css['feedback-item'])}>
-        <p className={clsx(css['feedback-text'])}>
-          Positive: {Math.round((good / totalFeedback) * 100)}%
-        </p>
+      <li
+        className={clsx(css['feedback-item'])}
+        style={{
+          background: `linear-gradient(to right, rgba(0, 122, 90, 0.85) ${positive}%, rgba(184, 41, 22, 0.85) ${positive}%)`,
+        }}
+      >
+        <p className={clsx(css['feedback-text'])}>Positive: {positive}%</p>
       </li>
     </ul>
   );
